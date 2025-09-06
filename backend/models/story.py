0dc7a719edcb72eec16d4096bce8745366f7f969
Daksh <dakshdaksh698@gmail.com>
2025-09-06 16:59:20 +0530

    Models and Schema Setup, BackendSetup Complete

---
+++ backend/models/story.py
@@ -0,0 +1,39 @@
+
+"""
+Example Story:
+Story Name
+theme
+first option
+children of the option: [go left,go right]
+
+text
+then more options
+This looks like a binary tree
+it is a branching pattern
+"""
+
+from sqlalchemy import Column, Integer, String, DateTime, Boolean,ForeignKey, JSON #ORM -> Object Relational Mapping(Mapping data to classes)
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+from db.database import Base
+
+
+class Story(Base):
+    __tablename__ = "stories"
+    id = Column(Integer, primary_key = True, index = True)
+    title = Column(String,index = True)
+    created_at = Column(DateTime(timezone=True),server_default= func.now())
+    nodes = relationship("StoryNode", back_populates="story")
+
+
+class StoryNode(Base):
+    __tablename__ = "story_nodes"
+    id = Column(Integer,primary_key = True, index = True)
+    story_id = Column(Integer, ForeignKey("stories.id"),index = True)
+    Content = Column(String)
+    is_root = Column(Boolean,default = False)
+    is_ending = Column(Boolean, default = False)
+    is_winning_ending = Column(Boolean,default = False)
+    options = Column(JSON, default = list)
+
+    story = relationship("Story",backpopulates="nodes")
