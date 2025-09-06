606a620999f94d23ee85590c4b6978e4e1261886
Daksh <dakshdaksh698@gmail.com>
2025-09-06 16:14:15 +0530

    FastAPI Setup with CORSMiddleWare

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware #What is CORSMiddleware
 
+app = FastAPI(
+    title = "Choose Your Own Adventure API",
+    Description = "API to cure boredom",
+    version = "0.1.0",
+    docs = "/docs",
+    redoc_url = "/redoc"
+)
+#Automatic documentation due to FastAPI
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True, #Allowing credentials
+    allow_methods = ["*"], #Allowing all methods(GET,POST,PUT,PATCH etc.)
+    allow_headers = ["*"], #Additional Information(Generally limited, but as base case currently allowing everything)
+)
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
